887b8b7e654f22d17d3762f7b9afb81cfb4bc6cc
Dawn-Technologies <85081929+Dawn-Technologies@users.noreply.github.com>
2021-05-31 10:22:50 +0100

    Added Code
    Added Code

---
+++ DawnOS/DawnOS/Kernel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace DawnOS
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        // Just started project basic kernel.
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("DawnOS booted successfully. Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+        }
+    }
+}
