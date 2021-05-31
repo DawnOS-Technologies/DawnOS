1f3e08cc2121738f351536a74f0563569985d801
Dawn-Technologies <85081929+Dawn-Technologies@users.noreply.github.com>
2021-05-31 10:35:20 +0100

    Create DawnUpdate.cs

---
+++ DawnOS/DawnOS/System/DawnUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DawnOS.System
+{
+    class DawnUpdate
+    {
+        public static void InstallBundle(string filename)
+        {
+            Directory.CreateDirectory(@"0:\system\updatetemp");
+            Tar.ExtractTar(filename, @"0:\system\updatetemp");
+            foreach (var upd in Directory.GetFiles(@"0:\system\updatetemp"))
+            {
+                Install(upd);
+            }
+        }
+        public static void Install(string filename)
+        {
+            Tar.ExtractTar(filename, @"0:\");
+        }
+    }
+}
