05c662cd5d322d0185191bf1468d2221285ad7e7
Dawn-Technologies <85081929+Dawn-Technologies@users.noreply.github.com>
2021-05-31 10:44:04 +0100

    Built

--- DawnOS/DawnOS/Kernel.cs
+++ DawnOS/DawnOS/Kernel.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
+using Cosmos.HAL;
 
 namespace DawnOS
 {
-    public class Kernel : Sys.Kernel
+    public unsafe class Kernel : Sys.Kernel
     {
 
+        private static byte* ProcessMemory;
+        private static int ProcessMemorySize;
+        public static string CurrentDir = "";
+        public static string CurrentVol = "0";
+        public static Sys.FileSystem.CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
+        public static int BootMode = 0;
+        
+        
         // Just started project basic kernel.
         protected override void BeforeRun()
         {
---
+++ DawnOS/DawnOS/System/User/UserProfileSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DawnOS.System.User
+{
+    public class UserProfileSystem
+    {
+        public static string CurrentUser { get; private set; }
+
+    }
+}
